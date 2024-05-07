bc9bc9eacf931812ef5cd72ae740b4a97dc02c32
AlainLobato <lobatoa829@gmail.com>
2024-05-06 20:27:40 -0600

    Revert "Reapply "Practica 12""
    This reverts commit 929d12bac7f797fc5fc7a6d4701bab7727ce3b91.

---
+++ myApp-2048298/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myApp-2048298',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
